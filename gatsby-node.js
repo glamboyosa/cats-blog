2edfc86fdd381027795e4bbcbafa388b6bdb0133
unknown <ogbemudiatimothy@gmail.com>
2019-09-11 23:52:45 +0100

    modified gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -25,10 +25,12 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
       return Promise.reject(res.errors)
     }
     res.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: postTemplate,
-      })
+      if (node.frontmatter.path) {
+        createPage({
+          path: node.frontmatter.path,
+          component: postTemplate,
+        })
+      }
     })
   })
 }
