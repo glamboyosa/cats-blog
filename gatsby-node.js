f21bf86eb58fd5dc43e4fc194d0ec157fdc9a99d
unknown <ogbemudiatimothy@gmail.com>
2019-09-30 18:36:17 +0100

    gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 const path = require("path")
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators
-  const postTemplate = path.resolve("src/templates/blog-post.js")
+  const postTemplate = path.resolve("./src/templates/blog-post.js")
   return graphql(`
     {
       allMarkdownRemark {
